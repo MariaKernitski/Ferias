84939d442e8b6b2e9d77b73b9a68c376f0cbbeb9
Maria <kernitskimaria@gmail.com>
2024-07-22 11:20:30 -0300

    feat: CRUD completo

---
+++ módulos.js
@@ -0,0 +1,62 @@
+const prompt = require("prompt-sync")();
+
+const atividades = [];
+
+const modelo = () => {
+    let atividade = {
+        dia: undefined,
+        descriçao: []
+    };
+
+    atividade.dia = prompt("Qual o dia? ");
+    let resposta;
+
+    while(true) {
+        resposta = prompt("O que fez nesse dia? ")
+
+        if(resposta == "fim") {
+            break;
+        }
+        else {
+            atividade.descriçao.push(resposta);
+        }
+    }
+    return atividade;
+}
+
+const criar = () => {
+
+    let atividade = modelo();
+
+    atividades.push(atividade);
+
+    console.log("Atividade cadastrada com sucesso!")
+}
+
+const listar = () => {
+    atividades.forEach((atividade, indice) => {
+        console.log(`${indice + 1}. ${atividade}`)
+    })
+}
+
+const atualizar = () => {
+    listar();
+
+    let indice = prompt("Qual índice deseja atualizar? ")
+
+    let atividade = modelo()
+
+    atividades[--indice] = atividade;
+
+    console.log("Índice atualizado com sucesso!")
+}
+
+const remover = () => {
+    listar();
+
+    let indice = prompt("Qual índice deseja remover? ")
+
+    atividades.splice(--indice, 1);
+
+    console.log("Índice removido com sucesso!")
+}
